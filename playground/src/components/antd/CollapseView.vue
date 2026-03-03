cc0d6b6b01ffd8f1d2a8b6321712604e3b2f7f54
jinsong <jinsong.che@ly.com>
2026-03-03 13:14:03 +0800

    refactor: Update CollapseView to use v-model for activeKey binding

--- playground/src/components/antd/CollapseView.vue
+++ playground/src/components/antd/CollapseView.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="padding: 8px 0">
-    <ACollapse :activeKey="activeKeys">
+    <ACollapse v-model:activeKey="activeNames">
       <ACollapsePanel v-for="(item, i) in items" :key="String(i)" :header="item.title">
         <div style="line-height: 1.6">{{ item.content }}</div>
       </ACollapsePanel>
@@ -9,12 +9,21 @@
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from "vue";
 import { Collapse as ACollapse } from "ant-design-vue";
 
 const ACollapsePanel = ACollapse.Panel;
 
-defineProps<{
+const props = defineProps<{
   items: readonly { title: string; content: string }[];
   activeKeys: readonly string[];
 }>();
+
+const activeNames = ref<string[]>([...props.activeKeys]);
+watch(
+  () => props.activeKeys,
+  (next) => {
+    activeNames.value = [...next];
+  },
+);
 </script>
